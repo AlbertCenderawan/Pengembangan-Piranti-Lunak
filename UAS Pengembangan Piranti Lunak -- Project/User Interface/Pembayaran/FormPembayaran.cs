b3031b15fc1955820d0f8fed3b3d0db819489c63
AlbertCenderawan <03082190015@student.uph.edu>
2021-03-25 10:59:37 +0700

    Form Pembayaran using C#

---
+++ UAS Pengembangan Piranti Lunak -- Project/User Interface/Pembayaran/FormPembayaran.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerjalananPenerbangan
+{
+    public partial class FormPembayaran : Form
+    {
+        public FormPembayaran()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTransfer_Click(object sender, EventArgs e)
+        {
+            string pemberitahuan;
+
+            pemberitahuan = "Virtual Account: 01010121233445 \n" +
+                "Jika sudah selesai, akan mendapat bon dan tiket dari e-mail. \n" +
+                "Jika belum melakukan pembayaran dalam jangka waktu 15 menit, maka booking akan hangus. \n";
+
+            MessageBox.Show(pemberitahuan);
+        }
+
+        private void btnTunai_Click(object sender, EventArgs e)
+        {
+            string pemberitahuan;
+
+            pemberitahuan = "Metode Pembayran : tunai. \n" +
+                "Pembayaran akan diserahkan kepada front-desk airport. \n" +
+                "Cek bon di dalam e-mail. \n";
+
+            MessageBox.Show(pemberitahuan);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            FormJadwalPenerbangan formJadwalPenerbangan = new FormJadwalPenerbangan();
+            formJadwalPenerbangan.Show(); this.Hide();
+        }
+    }
+}
